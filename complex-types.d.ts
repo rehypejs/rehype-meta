9c6f1828b79ea3dcbfbb3695c1f8bf27fe548169
Titus Wormer <tituswormer@gmail.com>
2022-11-08 18:51:20 +0100

    Fix typo

--- complex-types.d.ts
+++ complex-types.d.ts
@@ -152,7 +152,7 @@ declare module 'vfile' {
        *
        * Used by `rehype-meta`.
        */
-      images?: string | Image | Array<string | Image>
+      image?: string | Image | Array<string | Image>
 
       /**
        * Date the document (or site) was first published.
@@ -332,7 +332,7 @@ declare module 'vfile' {
        *
        * Used by `rehype-meta`.
        */
-      tags?: string | Image | Array<string | Image>
+      image?: string | Image | Array<string | Image>
 
       /**
        * Date the document (or site) was first published.
