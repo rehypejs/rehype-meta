217a672f8b002937354f4ea8e74b4012557c1b90
Titus Wormer <tituswormer@gmail.com>
2024-07-17 15:19:45 +0200

    Fix type extension now `Options` from `vfile` is an interace
    Related-to: vfile/vfile@7a42da4.

--- index.d.ts
+++ index.d.ts
@@ -1,10 +1,12 @@
+import type {Options as RehypeMetaFields} from './lib/types.js'
+
 export type {Image, Options} from './lib/types.js'
 export {default} from './lib/index.js'
 
 // Add custom data supported when `rehype-meta` is added.
 declare module 'vfile' {
-  interface DataMapMatter extends Options {}
-  interface DataMapMeta extends Options {}
+  interface DataMapMatter extends RehypeMetaFields {}
+  interface DataMapMeta extends RehypeMetaFields {}
 
   interface DataMap {
     matter: DataMapMatter
